354ecded3fa63f6aee1f97ec16f2b674f626b6ad
jeya01 <109455742+jeya01@users.noreply.github.com>
2022-09-06 02:41:09 +0530

    list remove duplicates

---
+++ array_q10.py
@@ -0,0 +1,31 @@
+
+"""
+You are a passport issuer, but due to some problems in the system, there are redundant  passport numbers. Your task is to delete all the duplicate passport numbers. You are given a list of passport numbers.
+
+Input Description:
+You are given length of list.Second line,You are given with a list.
+
+Output Description:
+Print the list of passport numbers without duplicates.
+
+Sample Input :
+5
+A23 B56 B56 C79 D16
+Sample Output :
+A23 B56 C79 D16
+
+
+"""
+
+n = int(input())
+
+#lt = list(map(str,input().split()))
+lt = [str(x) for x in input().split()]
+print(lt)
+
+ans = []
+
+for i in lt:
+    if i not in ans:
+        ans.append(i)
+print(*ans)
